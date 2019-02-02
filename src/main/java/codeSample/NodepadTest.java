d39dc97921ad99b6cb9c67327ab676d7377bdd75
saurav <sauravpd58@gmail.com>
2019-02-03 00:44:15 +0530

    added javadoc

--- src/main/java/codeSample/NodepadTest.java
+++ src/main/java/codeSample/NodepadTest.java
@@ -5,11 +5,21 @@ import java.net.URL;
 import org.openqa.selenium.winium.DesktopOptions;
 import org.openqa.selenium.winium.WiniumDriver;
 import org.testng.annotations.Test;
-
+/**
+ * NodepadTest using winium
+ * @author saurav
+ *
+ */
 public class NodepadTest 
 {
+	/**
+	 * application path
+	 */
 	public static final String APPLICATION_PATH="C:\\WINDOWS\\system32\\notepad.exe";
 	
+	/**
+	 * Sample window application test using winium
+	 */
 	@Test
 	public void test() 
 	{
@@ -22,7 +32,8 @@ public class NodepadTest
 			driver.findElementByClassName("Edit").sendKeys("This is sample window application test using winium");
 			driver.close();
 		}
-		catch(Exception e){
+		catch(Exception e)
+		{
 			System.out.println(e.getMessage());
 		}
 	}
